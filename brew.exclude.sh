be1ad60ac561c6356acb1ef434ee32b1f01db752
trystan2k <trystan2k@gmail.com>
2019-12-08 12:01:58 +0100

    feat(brew): update to install OhMyZSH in background

--- brew.exclude.sh
+++ brew.exclude.sh
@@ -13,12 +13,15 @@ if [ ! "$?" -eq 0 ] ; then
 fi
 
 # Make sure we’re using the latest Homebrew
+echo "Update Homebrew"
 brew update
 
 # Upgrade any already-installed formulae
+echo "Upgrade Homebrew"
 brew upgrade
 
 # Core Utils
+echo "Install coreutils"
 brew install coreutils
 
 # ---------------------------------------------
@@ -26,12 +29,15 @@ brew install coreutils
 # ---------------------------------------------
 
 # Git
+echo "Install git"
 brew install git
 
 # NVM
+echo "Install nvm"
 brew install nvm
 
 # Python 3
+echo "Install python"
 brew install python
 
 # ---------------------------------------------
@@ -39,9 +45,11 @@ brew install python
 # ---------------------------------------------
 
 # Yarn
-brew install yarn --without-node --ignore-dependencies
+echo "Install Yarn"
+brew install yarn --ignore-dependencies
 
 # Docker for containerization
+echo "Install docker"
 brew cask install docker
 
 # ---------------------------------------------
@@ -49,12 +57,15 @@ brew cask install docker
 # ---------------------------------------------
 
 # Make requests with awesome response formatting
+echo "Install httpie"
 brew install httpie
 
 # Show directory structure with excellent formatting
+echo "Install tree"
 brew install tree
 
 # Hyper terminal
+echo "Install hyper"
 brew cask install hyper
 
 # ---------------------------------------------
@@ -62,21 +73,27 @@ brew cask install hyper
 # ---------------------------------------------
 
 # Zsh 
+echo "Install zsh"
 brew install zsh
 
 # ZSH Plugins
+echo "Install zsh-autosuggestions zsh-syntax-highlighting thefuck autojump"
 brew install zsh-autosuggestions zsh-syntax-highlighting thefuck autojump
 
 # OhMyZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "Install Oh-My-ZSH"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # zsh-syntax-highlighting
+echo "Install zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # zsh-autosuggestions
+echo "Install zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # powerlevel9k
+echo "Install powerlevel9k"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # The Fire Code font
@@ -84,20 +101,25 @@ git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/theme
 # This method of installation is
 # not officially supported, might install outdated version
 # Change font in terminal preferences
+echo "Install Fira Code"
 brew tap caskroom/fonts
 brew cask install font-fira-code
 
 # Nerd Font
+echo "Install Nerd Font"
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
 
 # My favorite text editor
+echo "Install VS Code"
 brew cask install visual-studio-code
 
 # terminal-notifier
+echo "Install Terminal Notifier"
 brew install terminal-notifier
 
 # fzf
+echo "Install FZF"
 brew install fzf
 
 # ---------------------------------------------
@@ -105,4 +127,5 @@ brew install fzf
 # ---------------------------------------------
 
 # Remove outdated versions from the cellar
+echo "Brew Cleanup"
 brew cleanup
