dcbb87e17fa3960e7cce0c5e97e541051609bbf7
trystan2k <trystan2k@gmail.com>
2019-12-09 12:18:50 +0100

    feat(.hyper): remove not used settings, rever to webgl render and remove plugins not used anymore

--- .hyper.js
+++ .hyper.js
@@ -4,35 +4,17 @@
 
 module.exports = {
   config: {
-    webGLRenderer: false, 
+    webGLRenderer: true, 
     
     // choose either `'stable'` for receiving highly polished,
     // or `'canary'` for less polished but more frequent updates
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 14,
+    fontSize: 12,
 
     // font family with optional fallbacks
     fontFamily: 'Fira Code, "Hack Nerd Font", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
-
-    // default font weight: 'normal' or 'bold'
-    fontWeight: 'bold',
-
-    // font weight for bold characters: 'normal' or 'bold'
-    fontWeightBold: 'bold',
-
-    // line height as a relative unit
-    lineHeight: 1,
-
-    // letter spacing as a relative unit
-    letterSpacing: 0,
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(95,95,95,0.8)',
-
-    // terminal text color under BLOCK cursor
-    cursorAccentColor: '#000',
 
     // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
     cursorShape: 'BLOCK',
@@ -40,95 +22,8 @@ module.exports = {
     // set to `true` (without backticks and without quotes) for blinking cursor
     cursorBlink: true,
 
-    // color of the text
-    foregroundColor: '#f4f4f4',
-
-    // terminal background color
-    // opacity is only supported on macOS
-    backgroundColor: '#000',
-
-    // terminal selection color
-    selectionColor: 'rgba(82,82,82,0.7)',
-
-    // border color (window, tabs)
-    borderColor: '#fff',
-
-    // custom CSS to embed in the main window
-    css: ``,
-
-    // custom CSS to embed in the terminal window
-    termCSS: ``,
-
-    // if you're using a Linux setup which show native menus, set to false
-    // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: '',
-
-    // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: '',
-
     // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
-
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    colors: {
-      black: '#000000',
-      red: '#ab1500',
-      green: '#39fd35',
-      yellow: '#a9a600',
-      blue: '#36a5fc',
-      magenta: '#c12dbf',
-      cyan: '#00b3bf',
-      white: '#cacaca',
-      lightBlack: '#797979',
-      lightRed: '#ec2100',
-      lightGreen: '#00db00',
-      lightYellow: '#eae600',
-      lightBlue: '#36a5fc',
-      lightMagenta: '#ec3aea',
-      lightCyan: '#00e8ea',
-      lightWhite: '#eaeaea',
-    },
-
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // PowerShell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
-
-    // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
-    // by default `['--login']` will be used
-    shellArgs: ['--login'],
-
-    // for environment variables
-    env: {},
-
-    // set to `false` for no bell
-    bell: 'SOUND',
-
-    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
-    copyOnSelect: false,
-
-    // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
-    defaultSSHApp: true,
-
-    // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
-    // selection is present (`true` by default on Windows and disables the context menu feature)
-    // quickEdit: true,
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
+    padding: '2px 8px 14px 8px',
 
     // for advanced config flags please refer to https://hyper.is/#cfg  
 
@@ -137,6 +32,7 @@ module.exports = {
       border: true,
       tabIconsColored: true,
       closeAlign: "right",
+      activityColor: 'salmon',
     }, 
 
     hyperline: {                                                            
@@ -146,28 +42,6 @@ module.exports = {
         "memory",                                                                   
         "battery"
       ]                                                                          
-    },
-
-    hyperNative: {
-      darkMode: true,
-      colors: {
-          black: "#000000",
-          red: "#ab1500",
-          green: "#39fd35",
-          yellow: "#a9a600",
-          blue: "#36a5fc",
-          magenta: "#c12dbf",
-          cyan: "#00b3bf",
-          white: "#cacaca",
-          lightBlack: "#797979",
-          lightRed: "#ec2100",
-          lightGreen: "#00db00",
-          lightYellow: "#eae600",
-          lightBlue: "#36a5fc",
-          lightMagenta: "#ec3aea",
-          lightCyan: "#00e8ea",
-          lightWhite: "#eaeaea",
-      }
     }
   },
 
@@ -186,18 +60,14 @@ module.exports = {
     "hyperlinks",
     "hyperterm-safepaste",
     "hyper-search",
-    //"hyper-letters",
     "hyper-fading-scrollbar", 
-    "hyper-font-ligatures",
-    "hyper-reorderable-tabs"
+    "hyper-font-ligatures"
   ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: [
-    //"hyper-native",
-  ],
+  localPlugins: [],
 
   keymaps: {
     // Example
