6944eb41140d65a9444aca710646bc927742d251
trystan2k <trystan2k@gmail.com>
2019-12-14 16:19:45 +0100

    feat(tools-linux): remove verification of already apt installed package

--- scripts/tools-linux.sh
+++ scripts/tools-linux.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 install() {
-    command -v "${1}" > /dev/null 2>&1
-    if [ $? -ne 0 ] ; then
-        echo "Installing: ${1}..."
-        sudo apt install -y $1 $2
-    else
-        echo "Already installed: ${1}"
-    fi
+    echo "Installing: ${1}..."
+    sudo apt install -y $1 $2
 }
 
 installAppImage() {
