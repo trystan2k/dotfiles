5f7fce643ad96e415ca088f38fc61b0a50cb3600
trystan2k <trystan2k@gmail.com>
2019-12-14 11:57:09 +0100

    feat(tools-linux): fix hyper installation url

--- scripts/tools-linux.sh
+++ scripts/tools-linux.sh
@@ -101,7 +101,7 @@ sudo apt install -y firefox
 
 # Hyper terminal
 echo "Install hyper"
-installDebApp Hyper https://hyper-updates.now.sh/download/linux_deb
+installDebApp Hyper https://releases.hyper.is/download/deb
 
 # Sublime
 echo "Install Sublime"
@@ -109,7 +109,7 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
+install sublime-text
 
 # ---------------------------------------------
 # ZSH and Oh-My-ZSH
