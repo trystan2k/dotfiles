bbbccf80c7b17be36e62a9aea378ee6f4a67ff57
trystan2k <trystan2k@gmail.com>
2019-12-17 00:16:23 +0100

    feat(asdf.sh): add instalation of current nodejs version

--- tools/asdf.sh
+++ tools/asdf.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+CURRENT_NODE_VERSION=8.9.4
+
 # Install nodejs plugin
 asdf plugin-add nodejs
 
 # Add keys for nodejs
 sh ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+
+# Install current node version
+asdf install nodejs $CURRENT_NODE_VERSION
+
+# Set current node version as global version
+asdf global nodejs $CURRENT_NODE_VERSION
