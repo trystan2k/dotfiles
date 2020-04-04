3fd8f06c7538560b8cccceb0d56da7a0ebbd553a
trystan2k <trystan2k@gmail.com>
2020-04-04 15:02:44 +0200

    feat(tools): add fix zsh permissions call

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -72,6 +72,12 @@ cleanup() {
 # Configure tools
 # ---------------------------------------------
 configure() {
+
+    info "Fix ZSH configuration"
+    if ask_question 'Do you want to fiz ZSH permissions?'; then
+        fixZshPerms
+    fi
+
     info "ASDF Configuration"
 
     if ask_question 'Do you want to configure ASDF and install plugins?'; then
