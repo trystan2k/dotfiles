3b5f1ce3b71b22ba7511ce5981ee04c7706c53a2
trystan2k <trystan2k@gmail.com>
2020-08-10 07:50:18 +0200

    feat(tools,upgrades): add iina and remove zepling tools

---
+++ upgrades/from-8.5.0-to-latest.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# Export dotfiles folder
+DOTFILES_FOLDER="$(pwd | grep -o '.*dotfiles')"
+
+#shellcheck source=/dev/null
+source "$DOTFILES_FOLDER"/symlinks/.exports
+
+# Load functions
+#shellcheck source=/dev/null
+source "$DOTFILES_FOLDER"/lib/functions
+
+_remove() {
+    # ---------------------------------------------
+    # Uninstall not needed anymore
+    # ---------------------------------------------
+
+    info "Remove step"
+    brew uninstall --ignore-dependencies node
+
+    info "Uninstall zeplin"
+    brew cask uninstall zeplin
+}
+
+_add() {
+    # ---------------------------------------------
+    # Install new tools
+    # ---------------------------------------------
+
+    info "Add step"   
+
+    info "Install iina"
+    brew cask install iina
+}
+
+_configure() {
+    # ---------------------------------------------
+    # Install new tools
+    # ---------------------------------------------
+
+    info "Configure step"
+
+}
+
+_cleanup () {
+    # ---------------------------------------------
+    # Cleanup and finish
+    # ---------------------------------------------
+
+    info "Cleanup step"
+
+    #shellcheck source=/dev/null
+    . "$DOTFILES_FOLDER"/macos/cleanup.sh
+}
+
+execute() {
+    _remove
+    _add
+    _configure
+    _cleanup
+}
+
+execute 2>&1 | tee -a "$DOTFILE_LOG_FILE"
