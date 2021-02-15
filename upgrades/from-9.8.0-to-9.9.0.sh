1de256656c1013b629a07ebe6fe8de226c606498
Thiago Mendonca <trystan2k@gmail.com>
2021-02-15 07:23:24 +0100

    feat(tools): add meetingbar and pock tools

---
+++ upgrades/from-9.8.0-to-9.9.0.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Export dotfiles folder
+DOTFILES_FOLDER="$(pwd | grep -o '.*dotfiles')"
+
+#shellcheck source=/dev/null
+source "$DOTFILES_FOLDER"/symlinks/.exports
+
+# Load functions
+#shellcheck source=/dev/null
+source "$DOTFILES_FOLDER"/lib/functions
+
+_remove() {
+    # ---------------------------------------------
+    # Uninstall not needed anymore
+    # ---------------------------------------------
+
+    info "Remove step"
+    brew uninstall --ignore-dependencies node
+}
+
+_add() {
+    # ---------------------------------------------
+    # Install new tools
+    # ---------------------------------------------
+
+    info "Add step"
+
+    info "Install meetingbar"
+    brew install --cask meetingbar
+
+    info "Install pock"
+    brew install --cask pock
+}
+
+_configure() {
+    # ---------------------------------------------
+    # Install new tools
+    # ---------------------------------------------
+
+    info "Configure step"
+}
+
+_cleanup () {
+    # ---------------------------------------------
+    # Cleanup and finish
+    # ---------------------------------------------
+
+    info "Cleanup step"
+
+    #shellcheck source=/dev/null
+    . "$DOTFILES_FOLDER"/macos/cleanup.sh
+}
+
+execute() {
+    _remove
+    _add
+    _configure
+    _cleanup
+}
+
+execute 2>&1 | tee -a "$DOTFILE_LOG_FILE"
