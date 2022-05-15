05c2512997fde0947063376e6542aa1086d3ec0c
Thiago Mendonca <trystan2k@gmail.com>
2022-05-15 17:44:11 +0200

    feat(scripts): add postinstall script

---
+++ scripts/post-install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# dotfiles folder
+DOTFILES_FOLDER="$(pwd | grep -o '.*dotfiles')"
+
+# Load helper functions
+#shellcheck source=/dev/null
+source "$DOTFILES_FOLDER"/lib/functions
+#shellcheck source=/dev/null
+source "$DOTFILES_FOLDER"/symlinks/.exports
+
+# ---------------------------------------------
+# Post Install process
+# ---------------------------------------------
+
+postinstall() {
+    
+    info "Install Ruby Gems"
+    if ask_question 'Do you want to install Ruby Gems?'; then
+        #shellcheck source=/dev/null
+        . "$DOTFILES_FOLDER"/configure/ruby-gems.sh
+    fi   
+
+    info "Install NPM global tools"
+    if ask_question 'Do you want to install NPM global tools?'; then
+        #shellcheck source=/dev/null
+        . "$DOTFILES_FOLDER"/tools/npm-global-tools.sh
+    fi  
+}
+
+execute() {
+    postinstall
+}
+
+execute
