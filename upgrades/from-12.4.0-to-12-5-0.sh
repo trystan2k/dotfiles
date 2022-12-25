7d9463e69c4bb978562ff456a1e2aed8b65c86dc
Thiago Mendonca <trystan2k@gmail.com>
2022-12-25 12:08:41 +0000

    feat(tools): remove flameshot and add shottr

---
+++ upgrades/from-12.4.0-to-12-5-0.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Export dotfiles folder
+DOTFILES_FOLDER="$(pwd | grep -o '.*dotfiles')"
+
+#shellcheck source=/dev/null
+source "$DOTFILES_FOLDER"/symlinks/.exports
+
+# Load functions
+#shellcheck source=/dev/null
+source "$DOTFILES_FOLDER"/lib/functions
+
+_remove() {
+    # ---------------------------------------------
+    # Uninstall not needed anymore
+    # ---------------------------------------------
+
+    info "Remove step"
+    brew uninstall --ignore-dependencies node
+
+    info "Remove flameshot"
+    brew uninstall flameshot
+}
+
+_add() {
+    # ---------------------------------------------
+    # Install new tools
+    # ---------------------------------------------
+
+    info "Add step"
+
+    info "Install shottr"
+    brew install shottr
+}
+
+_configure() {
+    # ---------------------------------------------
+    # Install new tools
+    # ---------------------------------------------
+
+    info "Configure step"
+}
+
+_cleanup () {
+    # ---------------------------------------------
+    # Cleanup and finish
+    # ---------------------------------------------
+
+    info "Cleanup step"
+
+    #shellcheck source=/dev/null
+    . "$DOTFILES_FOLDER"/macos/cleanup.sh
+}
+
+execute() {
+    _remove
+    _add
+    _configure
+    _cleanup
+}
+
+execute 2>&1 | tee -a "$DOTFILE_LOG_FILE"
